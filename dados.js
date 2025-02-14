4fb740b5c7f71214c2dc959288aedf52a25c148f
atKw01 <106979650+atKw01@users.noreply.github.com>
2025-02-14 14:08:37 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,18 +1,28 @@
 var armazem = localStorage;
 
 function salvarDados() {
-    const valorAluno = document.getElementById("alunosTurma").value;
+    const valorAluno = document.getElementById("alunos").value;
     const valorFalta = document.getElementById("faltas").value;
     const valorTurma = document.querySelector("input[name='turma']:checked").value;
+    const valorClasse = document.querySelector("input[name='classe']:checked").value;
 
     turmaDados.turma.forEach((item) => {
         if (item.nome === valorTurma) {
-            item.alunos = valorAluno;
-            item.faltas = valorFalta;
+            item.alunos = Number(valorAluno);
+            item.faltas = Number(valorFalta);
+            item.classe = valorClasse;
         }
     });
     armazem.setItem("turmaDados", JSON.stringify(turmaDados));
-    //window.location.href = './exitcode.html';
+};
+function salvarData() {
+
+
+    turmaDados.dataPeroido.forEach((item) => {
+        item.dataInicio = valorDataInicio;
+        item.dataFim = valorDataFim;
+    });
+    armazem.setItem("turmaDados", JSON.stringify(turmaDados));
 };
 var turmaDados = {
     "turma": [
@@ -20,51 +30,65 @@ var turmaDados = {
             "nome": "sq3",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "qs3",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "tq3",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "sat2",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "sat3",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "sat5",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "pjeQuarta",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "pjeSabado",
             "alunos": 0,
             "faltas": 0,
+            "classe": "",
         },
         {
             "nome": "geral",
             "alunos": 0,
             "faltas": 0,
         },
+    ],
+    "dataPeroido": [
+        {
+            "dataInicio": "",
+            "dataFim": "",
+        },
     ]
 };
 
-document.getElementById("inputTurma").addEventListener("submit", function() {
+document.getElementById("inputTurma").addEventListener("submit", function(event) {
     event.preventDefault();
     salvarDados();
     console.log(turmaDados);
